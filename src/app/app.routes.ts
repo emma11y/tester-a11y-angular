a275bef956518260d7b462cb398e8516f3e857e3
Emmanuelle ABOAF <35635211+emma11y@users.noreply.github.com>
2025-04-25 17:32:32 +0200

    init project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: AccueilComponent,
+  },
+];
